2f138a7c89b2ae57ba35164489d5a2eb566289a4
yang-tang-ziran <yangxiao030911@outlook.com>
2023-12-01 16:55:35 +0800

    12/1

---
+++ file/file1.cpp
@@ -0,0 +1,11 @@
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    std::ofstream file;
+    file.open("C:\\C--\\file\\test.txt", ios::in | ios::out); // 使用ofstream打开文件
+    file << "已成功输入文字";                                 // 向文件中写入数据
+    file.close();                                             // 关闭文件
+}
